032a8ef4f273d5714fa0758629923d4416577308
Andra Pescaru <87182577+AndraPescaru@users.noreply.github.com>
2022-12-08 00:54:38 +0200

    am creat task 4

---
+++ src/lab2/task4/Book.java
@@ -0,0 +1,17 @@
+package lab2.task4;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String toString() {
+        return "Cartea " + this.title + " a fost scrisa de catre " + this.author + " in anul " + this.year;
+    }
+}
