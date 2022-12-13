c06954eb08634491eaad509f760872cefcc583e1
AndraPescaru <andrapescaru21@yahoo.com>
2022-12-13 17:18:41 +0200

    Incerc de pe laptopul de la serviciu

---
+++ src/lab1/task1/VeterinaryReport.java
@@ -0,0 +1,31 @@
+package lab1.task1;
+public class VeterinaryReport {
+
+    private int cats;
+    private int dogs;
+
+    public int getCats() {
+        return cats;
+    }
+
+    public int getDogs() {
+        return dogs;
+    }
+
+    public void setCats(int cats) {
+        this.cats = cats;
+    }
+
+    public void setDogs(int dogs) {
+        this.dogs = dogs;
+    }
+
+    public int getAnimalsCount() {
+        return dogs + cats;
+    }
+
+    public void displayStatistics() {
+
+        System.out.println("Total number of animals is " + getAnimalsCount());
+    }
+}
---
+++ src/lab1/task1/VeterinaryTest.java
@@ -0,0 +1,17 @@
+package lab1.task1;
+
+import lab1.task1.VeterinaryReport;
+public class VeterinaryTest {
+    public static void main(String[] args) {
+        VeterinaryReport vr = new VeterinaryReport();
+
+
+
+        
+        vr.setCats(99) ;
+        vr.setDogs(199) ;
+
+        vr.displayStatistics();
+        System.out.println("The class method says there are " + vr.getAnimalsCount() + " animals");
+    }
+}
