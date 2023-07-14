912c8f2d8558a8177f9b3b962c5897744e2537e8
Andra Pescaru <87182577+AndraPescaru@users.noreply.github.com>
2023-07-14 23:59:12 +0300

    implemented point d from 4th oop lab

--- src/lab4/database/Database.java
+++ src/lab4/database/Database.java
@@ -1,21 +1,36 @@
 package lab4.database;
-
+import java.util.Collections;
 import lab4.people.Student;
 import lab4.people.Teacher;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class Database {
-    private List<Student> students = new ArrayList<>();
-    private List<Teacher> teachers = new ArrayList<>();
+    private final List<Student> students = new ArrayList<>();
+    private final List<Teacher> teachers = new ArrayList<>();
+    private static int numberofinstance = 0;
 
     // TODO: make it Singleton
 
+    private static Database instance = null;
+
+    private Database() {
+        numberofinstance++;
+    }
+
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
     public static int getNumberOfInstances() {
-        // TODO
-        return 0;
+        return numberofinstance;
     }
 
     public void addTeachers(List<Teacher> teachers) {
@@ -28,31 +43,75 @@ public class Database {
 
     public List<Teacher> findTeachersBySubject(String subject) {
         // TODO
-        return null;
+        List<Teacher> lista = new ArrayList<>();
+
+        for (int i = 0; i < teachers.size(); i++) {
+            if (teachers.get(i).getSubjects().equals(subject)) {
+                lista.add(teachers.get(i));
+            }
+        }
+        return lista;
     }
 
     public List<Student> findAllStudents() {
         // TODO
-        return null;
+        return this.students;
     }
 
     public List<Teacher> findAllTeachers() {
         // TODO
-        return null;
+        return this.teachers;
     }
 
     public List<Student> getStudentsBySubject(String subject) {
         // TODO
-        return null;
+
+        List<Student> lista = new ArrayList<>();
+
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getSubjects().equals(subject)) {
+                lista.add(students.get(i));
+            }
+        }
+        return lista;
     }
 
     public List<Student> getStudentsByAverageGrade() {
         // TODO
-        return null;
+
+//        students.sort(new Comparator<>() {
+//            @Override
+//            public int compare(Student o1, Student o2) {
+//                return Double.compare(o1.averageGrade(), o2.averageGrade());
+//            }
+//        });
+
+//        students.sort((o1, o2) -> Double.compare(o1.averageGrade(), o2.averageGrade()));
+//
+//        students.sort(Comparator.comparingDouble(Student::averageGrade));
+
+
+//        for (int i = 0; i < students.size(); i++) {
+//            for (int j = i + 1; j < students.size(); j++) {
+//                if (students.get(i).averageGrade() > students.get(j).averageGrade()) {
+//                    Collections.swap(students,i,j);
+//                }
+//            }
+//        }
+
+        return students;
+
     }
 
     public List<Student> getStudentsByGradeForSubject(String subject) {
-        // TODO
-        return null;
+
+        students.sort(new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return Integer.compare(o1.getGradeForSubject(subject), o2.getGradeForSubject(subject));
+            }
+        });
+
+        return students;
     }
 }
